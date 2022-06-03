e41c7f6b69d1471c3baa2f758f54391360dd627d
Yevgen Yakovliev <yyakovliev02@gmail.com>
2022-06-03 14:23:33 +0300

    fix: refactored parser

--- engine/parser.go
+++ engine/parser.go
@@ -7,28 +7,31 @@ import (
 
 func Parse(in string) Command {
 	fields := strings.Fields(in)
-	if len(fields) >= 2 {
-		name := fields[0]
-		args := fields[1:]
-		if name == "print" {
-			text := strings.Join(args, " ")
-			return &PrintCommand{Arg: text}
-		} else if name == "add" {
-			arg1, err := strconv.Atoi(args[0])
-			if err != nil {
-				return &PrintCommand{Arg: "SYNTAX ERROR: " + err.Error()}
-			}
 
-			arg2, err := strconv.Atoi(args[1])
-			if err != nil {
-				return &PrintCommand{Arg: "SYNTAX ERROR: " + err.Error()}
-			}
-
-			return &AddCommand{Arg1: arg1, Arg2: arg2}
-		} else {
-			return &PrintCommand{Arg: "SYNTAX ERROR: Invalid command name (" + name + ")"}
-		}
+	if len(fields) < 2 {
+		return &PrintCommand{Arg: "SYNTAX ERROR: Not Enough Parameters"}
 	}
 
-	return nil
+	name := fields[0]
+	args := fields[1:]
+
+	switch name {
+	case "print":
+		message := strings.Join(args, " ")
+		return &PrintCommand{Arg: message}
+	case "add":
+		arg1, err := strconv.Atoi(args[0])
+		if err != nil {
+			return &PrintCommand{Arg: "SYNTAX ERROR: " + err.Error()}
+		}
+
+		arg2, err := strconv.Atoi(args[1])
+		if err != nil {
+			return &PrintCommand{Arg: "SYNTAX ERROR: " + err.Error()}
+		}
+
+		return &AddCommand{Arg1: arg1, Arg2: arg2}
+	default:
+		return &PrintCommand{Arg: "ERROR: Unkown Command (" + in + ")"}
+	}
 }
